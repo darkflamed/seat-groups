d8c63de113152bd1f641396a7d420ee09ff1d78b
Justin Mercer <darkflame9013@gmail.com>
2019-05-24 16:52:41 -0600

    Update seatgroups.sidebar.php
    Change name to Groups Manager

--- src/config/seatgroups.sidebar.php
+++ src/config/seatgroups.sidebar.php
@@ -3,12 +3,14 @@
 return [
     'seatgroups' => [
         'name'          => 'SeAT Groups',
+        'label'         => 'Groups Manager',
         'icon'          => 'fa-group',
         'route_segment' => 'seatgroups',
         'permission'    => 'seatgroups.view',
         'entries' => [
             [
                 'name'  => 'SeAT Groups',
+                'label' => 'Groups Manager',
                 'icon'  => 'fa-gear',
                 'route' => 'seatgroups.index',
                 'permission'    => 'seatgroups.view',
